bbd17ce10d34b46337f2a40b6e2d76f9a2529a48
Daniel-Ruiz-Gtz <58959667+Daniel-Ruiz-Gtz@users.noreply.github.com>
2024-01-29 12:49:04 -0600

    Update GUI

--- main.py
+++ main.py
@@ -5,6 +5,11 @@ def select_file():
     path = filedialog.askdirectory()
     path_label.config(text=path)
 
+def organize():
+    # Organize directory
+    
+    pass
+
 def toggle_mode():
     current_mode = mode_var.get()
     new_mode = "dark" if current_mode == "light" else "light"
@@ -19,8 +24,8 @@ def set_color_scheme():
     else:
         bg_color = "#333333"
         text_color = "white"
-    window_width = 500
-    window_height = 300
+    window_width = 550
+    window_height = 200
     screen_width = root.winfo_screenwidth()
     screen_height = root.winfo_screenheight()
     center_x = int(screen_width / 2 - window_width / 2)
@@ -31,28 +36,35 @@ def set_color_scheme():
     title.configure(bg=bg_color, fg=text_color)
     desc.configure(bg=bg_color, fg=text_color)
     path_label.configure(bg=bg_color, fg=text_color)
+    button_frame.configure(bg=bg_color)
 
-    # You might want to update more widgets here
+    # Widgets
 
 root = tk.Tk()
 root.title("OrganizeMe")
 
 mode_var = tk.StringVar()
-mode_var.set("dark")  # Default mode is dark
+mode_var.set("dark")
 
 mode_button = tk.Button(root, text="Mode", command=toggle_mode)
-mode_button.pack(pady=10, side="top", anchor="ne")
+mode_button.pack(pady=5, side="top", anchor="ne")
 
-title = tk.Label(root, text="Welcome to OrganizeMe", font=("Helvetica", 18))
+title = tk.Label(root, text="Welcome to OrganizeMe", font=("Helvetica", 18, "bold"))
 title.pack()
-desc = tk.Label(root, text="Select the directory to organize:", font=("Helvetica", 12))
+desc = tk.Label(root, text="Select the directory to organize:", font=("Helvetica", 14))
 desc.pack()
 
-path_label = tk.Label(root, text="", font=("Helvetica", 13))
+path_label = tk.Label(root, text="", font=("Helvetica", 11, "italic"))
 path_label.pack()
 
-path_button = tk.Button(root, text="Select", command=select_file)
-path_button.pack(ipadx=5, pady=15)
+button_frame = tk.Frame(root)
+button_frame.pack(pady=10)
+
+select_button = tk.Button(button_frame, text="Select", command=select_file)
+select_button.pack(side="left", padx=5)
+
+organize_button = tk.Button(button_frame, text="Organize", command=organize)
+organize_button.pack(side="left", padx=5)
 
 
 set_color_scheme()
